d005051d6d11bf57289d925286c217a77959ea7d
Hongyang-Wang <hongyangwang1992@gmail.com>
2016-09-02 10:53:40 -0400

    update

--- a1-2dfft/FourierTransform2D/fft2d.cc
+++ a1-2dfft/FourierTransform2D/fft2d.cc
@@ -17,6 +17,7 @@
 
 using namespace std;
 
+void Transform1D(Complex* h, int w, Complex* H);
 
 void Transform2D(const char* inputFN) 
 { // Do the 2D transform here.
@@ -38,8 +39,25 @@ void Transform2D(const char* inputFN)
   //   9a) If you are CPU 0, collect all values from other processors
   //       and print out with SaveImageData().
   InputImage image(inputFN);  // Create the helper object for reading the image
+  int w = image.GetWidth(), h = image.GetHeight();
   // Step (1) in the comments is the line above.
   // Your code here, steps 2-9
+  // Step (2)
+  // int numtasks, rank;
+  // MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  // printf ("Total number of tasks= %d. My rank= %d\n", numtasks, rank);
+  // Step (3)
+  Complex* outImg = new Complex[w * h];
+  // Step (4)
+  Complex* inImg = image.GetImageData();
+  // Step (5)
+  for (int i = 0; i < w * h; i += w) {
+  	Transform1D(inImg + i, w, outImg + i);
+  }
+
+  //
+  image.SaveImageData("1d.out.text", outImg, w, h);
 }
 
 void Transform1D(Complex* h, int w, Complex* H)
@@ -47,15 +65,30 @@ void Transform1D(Complex* h, int w, Complex* H)
   // Implement a simple 1-d DFT using the double summation equation
   // given in the assignment handout.  h is the time-domain input
   // data, w is the width (N), and H is the output array.
+  double coef = 2.0 * M_PI / w;
+  for (int n = 0; n < w; n++) {
+  	for (int k = 0; k < w; k++) {
+  	  H[n] = H[n] + h[k] * Complex(cos(coef * n * k), -sin(coef * n * k));
+  	}
+  }
 }
 
 int main(int argc, char** argv)
 {
   string fn("Tower.txt"); // default file name
   if (argc > 1) fn = string(argv[1]);  // if name specified on cmd line
-  // MPI initialization here
+
+  // MPI initialization here 
+  int rc = MPI_Init(&argc,&argv);
+  if (rc != MPI_SUCCESS) {
+    printf ("Error starting MPI program. Terminating.\n");
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+
   Transform2D(fn.c_str()); // Perform the transform.
+
   // Finalize MPI here
+  MPI_Finalize();
 }  
   
 
